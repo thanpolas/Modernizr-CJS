52e7c175594a76c8802df1145a263ab859839644
Thanasis Polychronakis <thanpolas@gmail.com>
2014-03-24 22:40:11 +0200

    CJS test/cookies

--- feature-detects/cookies.js
+++ feature-detects/cookies.js
@@ -9,25 +9,22 @@
 /* DOC
 Detects whether cookie support is enabled.
 */
-define(['Modernizr'], function( Modernizr ) {
-  // https://github.com/Modernizr/Modernizr/issues/191
+// https://github.com/Modernizr/Modernizr/issues/191
+module.exports = function(cb) {
+  // navigator.cookieEnabled is in IE9 but always true. Don't rely on it.
 
-  Modernizr.addTest('cookies', function () {
-    // navigator.cookieEnabled is in IE9 but always true. Don't rely on it.
-
-    // try..catch because some in situations `document.cookie` is exposed but throws a
-    // SecurityError if you try to access it; e.g. documents created from data URIs
-    // or in sandboxed iframes (depending on flags/context)
-    try {
-      // Create cookie
-      document.cookie = 'cookietest=1';
-      var ret = document.cookie.indexOf('cookietest=') != -1;
-      // Delete cookie
-      document.cookie = 'cookietest=1; expires=Thu, 01-Jan-1970 00:00:01 GMT';
-      return ret;
-    }
-    catch (e) {
-      return false;
-    }
-  });
-});
+  // try..catch because some in situations `document.cookie` is exposed but throws a
+  // SecurityError if you try to access it; e.g. documents created from data URIs
+  // or in sandboxed iframes (depending on flags/context)
+  try {
+    // Create cookie
+    document.cookie = 'cookietest=1';
+    var ret = document.cookie.indexOf('cookietest=') != -1;
+    // Delete cookie
+    document.cookie = 'cookietest=1; expires=Thu, 01-Jan-1970 00:00:01 GMT';
+    cb(ret);
+  }
+  catch (e) {
+    cb(false);
+  }
+};
