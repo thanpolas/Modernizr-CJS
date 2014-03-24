4193a6dca5a55efa2cb7550557d11ddae59ba6a9
Thanasis Polychronakis <thanpolas@gmail.com>
2014-03-24 22:31:33 +0200

    touch core

--- src/Modernizr.js
+++ src/Modernizr.js
@@ -1,14 +1,13 @@
-define(['ModernizrProto'], function( ModernizrProto ) {
-  // Fake some of Object.create
-  // so we can force non test results
-  // to be non "own" properties.
-  var Modernizr = function(){};
-  Modernizr.prototype = ModernizrProto;
+var ModernizrProto = require('./ModernizrProto');
 
-  // Leak modernizr globally when you `require` it
-  // rather than force it here.
-  // Overwrite name so constructor name is nicer :D
-  Modernizr = new Modernizr();
+// Fake some of Object.create
+// so we can force non test results
+// to be non "own" properties.
+var Modernizr = function(){};
 
-  return Modernizr;
-});
+Modernizr.prototype = ModernizrProto;
+
+// Leak modernizr globally when you `require` it
+// rather than force it here.
+// Overwrite name so constructor name is nicer :D
+module.exports = new Modernizr();
