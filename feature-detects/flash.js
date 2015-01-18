03c1793bb8ec1e7846a1df97541e3c7bffaf9c46
Thanasis Polychronakis <thanpolas@gmail.com>
2015-01-18 23:41:54 +0200

    update flash detect and getBody

--- feature-detects/flash.js
+++ feature-detects/flash.js
@@ -11,7 +11,20 @@ Detects support flash, as well as flash blocking plugins
 */
 var createElement = require('../src/createElement');
 var docElement = require('../src/docElement');
+var getBody = require('../src/getBody');
 
+
+/*!
+  {
+  "name": "Flash",
+  "property": "flash",
+  "tags": ["flash"],
+  "polyfills": ["shumway"]
+  }
+  !*/
+/* DOC
+Detects support flash, as well as flash blocking plugins
+*/
 var test = module.exports = function(cb) {
   /* jshint -W053 */
   var runTest = function(result, embed) {
@@ -20,14 +33,13 @@ var test = module.exports = function(cb) {
       bool = new Boolean(bool);
       bool.blocked = (result === 'blocked');
     }
-    if (embed) {
-      docElement.removeChild(embed);
+    if (embed && body.contains(embed)) {
+      body.removeChild(embed);
     }
-    cb(bool);
   };
   var easy_detect;
   var activex;
-  // we wrap activex in a try/catch becuase when flash is disabled through
+  // we wrap activex in a try/catch because when flash is disabled through
   // ActiveX controls, it throws an error.
   try {
     // Pan is an API that exists for flash objects.
@@ -43,11 +55,15 @@ var test = module.exports = function(cb) {
     // flash seems to be installed, but it might be blocked. We have to
     // actually create an element to see what happens to it.
     var embed = createElement('embed');
+    var body = getBody();
+    var blockedDetect;
     var inline_style;
 
     embed.type = 'application/x-shockwave-flash';
 
-    docElement.appendChild(embed);
+    // Need to do this in the body (fake or otherwise) otherwise IE8 complains
+    body.appendChild(embed);
+    docElement.appendChild(body);
 
     // Pan doesn't exist in the embed if its IE (its on the ActiveXObjeect)
     // so this check is for all other browsers.
@@ -56,27 +72,45 @@ var test = module.exports = function(cb) {
       return;
     }
 
+    blockedDetect = function() {
+      // if we used a fake body originally, we need to restart this test, since
+      // we haven't been attached to the DOM, and therefore none of the blockers
+      // have had time to work.
+      if (!docElement.contains(body)) {
+        body = document.body || body;
+        embed = document.createElement('embed');
+        embed.type = 'application/x-shockwave-flash';
+        body.appendChild(embed);
+        return setTimeout(blockedDetect, 1000);
+      }
+      if (!docElement.contains(embed)) {
+        runTest('blocked');
+      }
+      else {
+        inline_style = embed.style.cssText;
+        if (inline_style !== '') {
+          // the style of the element has changed automatically. This is a
+          // really poor heuristic, but for lower end flash blocks, it the
+          // only change they can make.
+          runTest('blocked', embed);
+        }
+        else {
+          runTest(true, embed);
+        }
+      }
+
+      // If we're rockin' an attached fake body, clean it up
+      if (body.fake && body.parentNode) {
+        body.parentNode.removeChild(body);
+      }
+    };
+
     // If we have got this far, there is still a chance a userland plugin
     // is blocking us (either changing the styles, or automatically removing
     // the element). Both of these require us to take a step back for a moment
     // to allow for them to get time of the thread, hence a setTimeout.
-    setTimeout(function() {
-      if (!docElement.contains(embed)) {
-        runTest('blocked');
-        return;
-      }
-
-      inline_style = embed.style.cssText;
-      if (inline_style !== '') {
-        // the style of the element has changed automatically. This is a
-        // really poor heuristic, but for lower end flash blocks, it the
-        // only change they can make.
-        runTest('blocked', embed);
-        return;
-      }
-
-      runTest(true, embed);
-    }, 10);
+    //
+    setTimeout(blockedDetect, 10);
   }
 };
 
--- src/getBody.js
+++ src/getBody.js
@@ -1,16 +1,12 @@
-define(['createElement'], function( createElement ) {
-  function getBody() {
-    // After page load injecting a fake body doesn't work so check if body exists
-    var body = document.body;
+module.exports = function getBody() {
+  // After page load injecting a fake body doesn't work so check if body exists
+  var body = document.body;
 
-    if(!body) {
-      // Can't use the real body create a fake one.
-      body = createElement('body');
-      body.fake = true;
-    }
-
-    return body;
+  if(!body) {
+    // Can't use the real body create a fake one.
+    body = createElement('body');
+    body.fake = true;
   }
 
-  return getBody;
-});
+  return body;
+};
